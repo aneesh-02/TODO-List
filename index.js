db903f34898eaae85795f792c8e8899da0b88269
unknown <aneeshsharmachd@gmail.com>
2021-07-27 19:28:40 +0530

    installed ejs and set up views engine

--- index.js
+++ index.js
@@ -14,4 +14,9 @@ app.listen(port, function(err)
 });
 
 
+// use express router 
+app.use("/", require("./routes"));
 
+// set up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 console.log("Router is loaded");
 
+const homeController = require("../controllers/home_controller");
+
 module.exports = router;
