dd74882d23b6546760ae6c246597d0b9fa6fa35a
unknown <aneeshsharmachd@gmail.com>
2021-07-29 11:24:12 +0530

    Fixed bugs

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const router = express.Router();
 console.log("Router is loaded");
 
 const homeController = require("../controllers/home_controller");
+router.get("/",homeController.home);
+
+// for any further routes acess from here
+// router.use("/routerName". require("./routerFile"));
 
 module.exports = router;
